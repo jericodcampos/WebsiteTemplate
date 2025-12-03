e43a020d731a8acde80109e09827b4fda61dfbbc
jericodcampos <124763319+jericodcampos@users.noreply.github.com>
2025-12-03 14:33:56 +0800

    Add files via upload

---
+++ ans/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>    
+<head>
+    <meta charset="UTF-8">
+    <title>ANS Technical Competency Program</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav>
+        <label class="logo">ANS TCC Platform</label>
+        <ul>
+            <li><a href="module1.php">Home</a></li>
+            <li><a href="module2.php">Contact</a></li>
+            <li><a href="module3.php">Admission</a></li>
+            <li><a href="module4.php" class="btn btn-success">Login</a></li>
+        </ul>
+    </nav> 
+    <div>
+        <img src="grapic_design.png" >
+    </div> 
+
+    <div class="section1">
+        <label class="img_text">TCC Ensures ATSEPs' Competence!!</label>
+        <img class="main_img" src="school.png">
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="playground.jpg" class="img-responsive">
+
+            </div>
+            <div class="col-md-8">
+                <h2>About TCC Program</h2>
+                <p>The ANS Technical Competency Certification (TCC) Program is designed to ensure that Air Traffic Safety Electronics Personnel (ATSEPs) possess the necessary skills and knowledge to perform their duties effectively. The program includes a series of assessments and training modules aimed at enhancing the technical competencies of ATSEPs.</p>
+
+            </div>            
+
+        </div>
+
+    </div>
+
+    <center>
+        <h1>Our Teachers</h1>
+    </center>
+
+    <div>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="teacher" src="teacher1.png">
+                <p>In an ever changing environment where performance of daily tasks is a success</p>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="teacher2.png">
+                <p>In an ever changing environment where performance of daily tasks is a success</p>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="teacher3.png">
+                <p>In an ever changing environment where performance of daily tasks is a success</p>
+            </div>
+        </div>
+    </div>
+
+    <center>
+        <h1>The Courses</h1>
+    </center>
+
+    <div>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="teacher" src="web_development.png">
+                <h1>Communications</h1>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="graphic_design.png">
+                <h1>Navigation</h1>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="digital_marketing.png">
+                <h1>Surveillance</h1>   
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="digital_marketing.png">
+                <h1>Data Systems</h1>   
+            </div> 
+            <div class="col-md-4">
+                <img class="teacher" src="digital_marketing.png">
+                <h1>Meteorological Instruments</h1>   
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="digital_marketing.png">
+                <h1>Airfield and Power Systems</h1>   
+            </div>           
+        </div>
+    </div>
+
+
+    <center>
+    <h1 class="adm">Admission</h1>
+    </center>
+    <div align="center" class="admission_form">
+        <form>
+            <div class="adm_int">
+                <label class="label_text">Name:</label>
+                <input class="input_deg" type="text" name="name">
+            </div>
+            <div class="adm_int">
+                <label class="label_text">Email:</label>
+                <input class="input_deg"  type="text" name="name">
+            </div>
+            <div class="adm_int">
+                <label class="label_text">Phone:</label>
+                <input class="input_deg"  type="text" name="name">
+            </div>
+            <div class="adm_int">
+                <label class="label_text">Message:</label>
+                <textarea class="input_txt"></textarea>
+            </div>
+            <div class="adm_int">
+                 <input class="btn btn-primary" id="submit" type="submit" value="Apply" >
+            </div>
+        </form>
+    </div>
+
+
+
+    <footer>
+        <h3 class="footer_txt">&copy; 2024 ANS Technical Competency Program. All rights reserved.</h3>
+    </footer>
+</body>
+</html>
